d8f72c34995b9fb7055030973c901a6885cd8386
shivani6898 <51701737+shivani6898@users.noreply.github.com>
2020-05-31 14:09:34 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/**first slider */
+$(".slider-one")
+.not(".slick-intialized")
+.slick(
+    {
+    autoplay:true,
+    autoplayspeed:3000,
+    dots:true,
+    prevArrow:".site-slider.slider-btn.prev",
+    nextArrow:".site-slider.slider-btn.next",
+
+
+    }
+)
+
+/**Second slider */
+$(".slider-two")
+.not(".slick-intialized")
+.slick(
+    {
+       
+    prevArrow:".site-slider-two.prev",
+    nextArrow:".site-slider-two.next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplayspeed:3000,
+
+    }
+)
